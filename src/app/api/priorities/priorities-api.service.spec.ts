20569d4cb47c0b4bbcc73218d1f4f8f1763452d5
David Barragán Merino <david.barragan@kaleidos.net>
2020-06-17 16:52:08 +0200

    feat(api): #130 priorities

---
+++ src/app/api/priorities/priorities-api.service.spec.ts
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2014-2020 Taiga Agile LLC
+ *
+ * This source code is licensed under the terms of the
+ * GNU Affero General Public License found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import { createHttpFactory, SpectatorHttp, HttpMethod } from '@ngneat/spectator';
+
+import { ConfigService } from '@/app/config.service';
+import { ConfigServiceMock } from '@/app/config.service.mock';
+import { PrioritiesApiService } from './priorities-api.service';
+import { PriorityMockFactory } from './priorities.model.mock';
+
+describe('PrioritiesApiService', () => {
+  const project = 1;
+
+  let spectator: SpectatorHttp<PrioritiesApiService>;
+  const createHttp = createHttpFactory({
+    service: PrioritiesApiService,
+    providers: [
+      { provide: ConfigService, useValue: ConfigServiceMock },
+    ],
+  });
+
+  beforeEach(() => spectator = createHttp());
+
+  it('list', () => {
+    spectator.service.list().subscribe();
+    spectator.expectOne(`${ConfigServiceMock.apiUrl}/priorities`, HttpMethod.GET);
+  });
+
+  it('list filtered by project', () => {
+    const queryParams = {
+      project: project.toString(),
+    };
+
+    spectator.service.list(project).subscribe();
+    spectator.expectOne(`${ConfigServiceMock.apiUrl}/priorities?${new URLSearchParams(queryParams)}`, HttpMethod.GET);
+  });
+
+  it('get', () => {
+    const id = 1;
+
+    spectator.service.get(id).subscribe();
+    spectator.expectOne(`${ConfigServiceMock.apiUrl}/priorities/${id}`, HttpMethod.GET);
+  });
+
+  it('create', () => {
+    const data = {
+      name: 'test',
+      color: '#fabada',
+      order: 1,
+      project: 1,
+    };
+
+    spectator.service.create(data).subscribe();
+    spectator.expectOne(`${ConfigServiceMock.apiUrl}/priorities`, HttpMethod.POST);
+  });
+
+  it('put', () => {
+    const id = 1;
+    const data = PriorityMockFactory.build({id});
+
+    spectator.service.put(id, data).subscribe();
+    spectator.expectOne(`${ConfigServiceMock.apiUrl}/priorities/${id}`, HttpMethod.PUT);
+  });
+
+  it('patch', () => {
+    const id = 1;
+    const data = {
+      color: '#fabada',
+    };
+
+    spectator.service.patch(id, data).subscribe();
+    spectator.expectOne(`${ConfigServiceMock.apiUrl}/priorities/${id}`, HttpMethod.PATCH);
+  });
+
+  it('delete', () => {
+    const id = 1;
+
+    spectator.service.delete(id).subscribe();
+    spectator.expectOne(`${ConfigServiceMock.apiUrl}/priorities/${id}`, HttpMethod.DELETE);
+  });
+});
